6029aee75178f399ce4c59acc15d103e69417709
Admin <dzedun007@yandex.ru>
2025-01-06 13:31:09 +0400

    Рабочая версия

--- module_2_2.py.py
+++ module_2_2.py.py
@@ -1,9 +1,9 @@
 first = input('Введите, пожалуйста, параметр first: ')
 second = input('Введите, пожалуйста, параметр second: ')
 third = input('Введите, пожалуйста, параметр third: ')
-if first==second and second==third and third==first:
-    print ('3')
+if first == second and second == third and third == first:
+    print('3')
 elif first == second or second == third or third == first:
-    print ('2')
+    print('2')
 else:
-    print ('0')
+    print('0')
