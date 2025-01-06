d6bcb1f4d278a6c67f2677f83914b5c0b755faa4
Admin <dzedun007@yandex.ru>
2025-01-06 13:30:22 +0400

    Рабочая версия

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = ([5, 3, 4], 0, 'string', False)
+print (immutable_var)
+#immutable_var [4] = True
+#print (immutable_var)
+#Элементы кортежя являются константами, изменить их нельзя.
+mutable_list = [2, 43, 68, 467]
+print (mutable_list)
+mutable_list[3]= 'sixteen'
+print (mutable_list)
